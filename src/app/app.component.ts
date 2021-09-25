710f5fdf00c9c26456905ef7ddc0e1bd0f3e704a
Adrian <adrianbravo145@gmail.com>
2021-09-25 14:39:26 -0300

    piano

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'piano';
+
+  aplicarSond(number: number) {
+    const audio = new Audio();
+    audio.src = `../assets/sonidos/note${number}.wav`;
+    audio.load();
+    audio.play();
+  }
 }
